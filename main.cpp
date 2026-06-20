8a831ec47096285b3520b20d64387b96a498ddcb
Tamanna <tamannamalik935@gmail.com>
2026-06-20 17:29:33 +0530

    Initialize StudyTracker project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* 
+Time TRACKER V1
+Features: 
+1. Add Task 
+2. View Tasks
+3. Save Tasks
+4. Load Tasks
+5. Exit
+
+Future Features:
+1. Start Timer
+2. Stop Timer
+3. Analytics
+4. Bar charts
+*/
